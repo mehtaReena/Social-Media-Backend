926633c6f238b5aaa96c45c85ca04ffe480cd867
mehtaReena <j.reena07@gmail.com>
2021-08-02 13:06:48 +0530

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/public')
+     cb(null, 'static/uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + "-" + file.originalname)
